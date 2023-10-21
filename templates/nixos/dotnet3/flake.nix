5b46f6ceaa5eb56c2e573b95dd2853c21d7ef558
Nicholas Mason <nmt.mason@googlemail.com>
2023-10-21 16:41:00 +0100

    feat: add x86_64 specific templates

---
+++ templates/nixos/dotnet3/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Default template for a .NET and JavaScript development environment.";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable-2211.url = "github:nixos/nixpkgs/nixos-22.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs-unstable,
+    nixpkgs-stable-2211,
+  }: let
+    overlays = [
+      (final: prev: rec {
+        nodejs = prev.nodejs-18_x;
+        pnpm = prev.nodePackages.pnpm;
+        yarn = prev.yarn.override {inherit nodejs;};
+      })
+    ];
+
+    system = "x86_64-linux";
+
+    unstable = import nixpkgs-unstable {inherit overlays system;};
+    stable-2211 = import nixpkgs-stable-2211 {inherit system;};
+
+    unstablePackages = with unstable; [nodejs-18_x pnpm yarn];
+    stable2211Packages = with stable-2211; [dotnet-sdk_3];
+  in {
+    devShells.${system}.default =
+      unstable.mkShell {packages = unstablePackages ++ stable2211Packages;};
+  };
+}
